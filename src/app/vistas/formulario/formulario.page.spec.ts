6f312cbb99a99de072ab1332da6b9ab4e2f83fcc
Edgardo Cayo <ecayo2@santotomas.cl>
2021-06-05 12:44:31 -0400

    formulario para registro

--- src/app/vistas/formulario/formulario.page.spec.ts
+++ src/app/vistas/formulario/formulario.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { FormularioPage } from './formulario.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('FormularioPage', () => {
+  let component: FormularioPage;
+  let fixture: ComponentFixture<FormularioPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ FormularioPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(FormularioPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
